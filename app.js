851a5b6ed0f43842d6b4f05781c7ee68fdcb781c
Luciano Júnior <luciannojunior@gmail.com>
2018-09-19 12:00:19 -0300

    Error handling

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const cheerio = require('cheerio')
 const app = express()
 
 const SEARCH_SONGS_API = 'https://studiosolsolr-a.akamaihd.net/cc/h2/'
-const getChordsApi = (artist, song) => `https://www.cifraclub.com.br/${artist}/${song}/imprimir.html`
-const getArtistApi = (artistSlug) => `https://www.vagalume.com.br/${artistSlug}/index.js`
+const getChordsApi = (artist, song) =>
+  `https://www.cifraclub.com.br/${artist}/${song}/imprimir.html`
+const getArtistApi = artistSlug =>
+  `https://www.vagalume.com.br/${artistSlug}/index.js`
+
+// Tells us that this entry is a song, and not an artist
+const CIFRACLUB_SONG_ID = '2'
 
 /**
  * Receives a part of a song name and search for songs in CifraClub
  */
-app.get('/songs', async ({ query: { name }}, res) => {
+app.get('/songs', async ({ query: { name } }, res) => {
   const response = await axios.get(SEARCH_SONGS_API, {
     params: { q: name },
   })
@@ -28,8 +33,7 @@ app.get('/songs', async ({ query: { name }}, res) => {
   const entries = processed.response.docs
 
   const finalResult = entries
-    // Tells us that this entry is a song, and not an artist
-    .filter(entry => entry.t === '2')
+    .filter(entry => entry.t === CIFRACLUB_SONG_ID)
     // Change properties from CifraClub to a more human-readable fancy
     .map(({ m, a, u, d }) => ({
       songName: m,
@@ -45,35 +49,43 @@ app.get('/songs', async ({ query: { name }}, res) => {
  * Get the chords of a specific song in the CifraClub service
  */
 app.get('/chords/:artist/:song', async ({ params: { artist, song } }, res) => {
-  const response = await axios.get(getChordsApi(artist, song))
-  const $ = cheerio.load(response.data)
-  $('.tablatura').remove()
-  return res.json($('pre').html())
+  try {
+    const response = await axios.get(getChordsApi(artist, song))
+    const $ = cheerio.load(response.data)
+    $('.tablatura').remove()
+    return res.json($('pre').html())
+  } catch (e) {
+    return res.status(404).json({})
+  }
 })
 
 /**
  * Recover data from an specific artist by its slug (we are using the genre)
- * 
+ *
  * It can also recover top lyrics if ?complete=true
  */
 app.get(
   '/artists/:artistslug',
   async ({ params: { artistslug }, query: { complete } }, res) => {
-    const response = await axios.get(getArtistApi(artistslug))
-    const artist = response.data.artist
+    try {
+      const response = await axios.get(getArtistApi(artistslug))
+      const artist = response.data.artist
 
-    if (!artist) return res.status(404).json({})
+      if (!artist) return res.status(404).json({})
 
-    let artistResponse = {
-      name: artist.desc,
-      imgUrl: artist.pic_small,
-      genre: artist.genre ? artist.genre[0].name : undefined,
+      let artistResponse = {
+        name: artist.desc,
+        imgUrl: artist.pic_small,
+        genre: artist.genre ? artist.genre[0].name : undefined,
+      }
+
+      if (complete) {
+        artistResponse = { ...artistResponse, topLyrics: artist.toplyrics.item }
+      }
+      return res.json(artistResponse)
+    } catch (e) {
+      return res.status(404).json({})
     }
-
-    if (complete) {
-      artistResponse = { ...artistResponse, topLyrics: artist.toplyrics.item }
-    }
-    return res.json(artistResponse)
   }
 )
 
